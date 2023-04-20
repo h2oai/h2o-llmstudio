2e65d63e8c1100232b3a013d72328b2c5e021120
maxjeblick <maximilianjeblick@gmail.com>
2023-04-20 12:22:50 +0200

    formatting

--- prompt.py
+++ prompt.py
@@ -92,7 +92,8 @@ if __name__ == "__main__":
     print()
     print("=============")
     print(
-        "You can change inference parameters on the fly by typing --param value, such as --num_beams 4. You can also chain them such as --num_beams 4 --top_k 30."
+        "You can change inference parameters on the fly by typing --param value, "
+        "such as --num_beams 4. You can also chain them such as --num_beams 4 --top_k 30."
     )
     print()
 
