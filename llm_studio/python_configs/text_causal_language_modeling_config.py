0c0542742959415ec30132c4b2b9832cc3fb62c3
maxjeblick <maximilianjeblick@gmail.com>
2023-04-25 16:37:44 +0200

    reintroduce add_prompt_answer_tokens

--- llm_studio/python_configs/text_causal_language_modeling_config.py
+++ llm_studio/python_configs/text_causal_language_modeling_config.py
@@ -190,6 +190,7 @@ class ConfigNLPCausalLMTokenizer(DefaultConfig):
     max_length_prompt: int = 256
     max_length_answer: int = 256
     max_length: int = 512
+    add_prompt_answer_tokens: bool = False
     padding_quantile: float = 1.0
     add_prefix_space: bool = False
 
