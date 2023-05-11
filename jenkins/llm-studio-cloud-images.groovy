984420f175a29a5e4e1b4f2eec13ce3c5f2ee62f
DinukaH2O <dinuka.appuhamy@h2o.ai>
2023-05-11 11:13:33 +0530

    ci:Azure packer build

--- jenkins/llm-studio-cloud-images.groovy
+++ jenkins/llm-studio-cloud-images.groovy
@@ -7,6 +7,7 @@ properties(
                 string(name: 'BRANCH_TAG', defaultValue: 'origin/main'),
                 booleanParam(name: 'AWS', defaultValue: true, description: 'Make Amazon Machine Image/Not?'),
                 booleanParam(name: 'GCP', defaultValue: true, description: 'Make GCP Image/Not?'),
+                booleanParam(name: 'AZURE', defaultValue: true, description: 'Make AZURE Image/Not?'),
                 string(name: 'LLM_STUDIO_VERSION')
             ]
         )
@@ -69,6 +70,31 @@ node('mr-0x16') {
                                 }
                             }
                         },
+
+                         "AZURE Ubuntu 20.04": {
+                            withCredentials([string(credentialsId: "AZURE_MARKETPLACE_CLIENT_ID", variable: "AZURE_CLIENT_ID"),
+                                             string(credentialsId: "AZURE_MARKETPLACE_CLIENT_SECRET", variable: "AZURE_CLIENT_SECRET"),
+                                             string(credentialsId: "AZURE_MARKETPLACE_SUBSCRIPTION_ID", variable: "AZURE_SUBSCRIPTION_ID"),
+                                             string(credentialsId: "AZURE_MARKETPLACE_TENANT_ID", variable: "AZURE_TENANT_ID")]) {
+                                dir('jenkins') {
+                                    if (params.AZURE) {
+                                        sh("packer build \
+                                            -var 'client_id=$AZURE_CLIENT_ID' \
+                                            -var 'client_secret=$AZURE_CLIENT_SECRET' \
+                                            -var 'managed_image_resource_group_name=H2OIMAGES' \
+                                            -var 'subscription_id=$AZURE_SUBSCRIPTION_ID' \
+                                            -var 'tenant_id=$AZURE_TENANT_ID' \
+                                            -var 'llm_studio_version=${LLM_STUDIO_VERSION}' \
+                                            llm-studio-azure.json"
+                                        )
+                                        archiveArtifacts artifacts: '*-image-info.json'
+                                    }else {
+                                        Utils.markStageSkippedForConditional('AZURE Ubuntu 20.04')
+                                    }
+                                }
+                            }
+                        },
+
                 ])
             }
         } finally {
