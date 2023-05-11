f2010cc57285be18003fa4aa7f0407872118573d
DinukaH2O <dinuka.appuhamy@h2o.ai>
2023-05-11 12:08:57 +0530

    ci:refactor script

--- jenkins/install_llm_studio_ubuntu2004.sh
+++ jenkins/install_llm_studio_ubuntu2004.sh
@@ -2,9 +2,9 @@
 
 # Install core packages
 sudo apt update
-# sudo apt install -y software-properties-common
-# sudo add-apt-repository universe -y
-# sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe -y
+sudo apt update
 sudo apt -y install curl
 sudo apt -y install make
 
@@ -18,15 +18,13 @@ sudo apt-get -y install python3.10-distutils
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 
 # Clone h2o-llmstudio
-
 git clone https://github.com/h2oai/h2o-llmstudio.git
 cd h2o-llmstudio
 
 # Create virtual environment (pipenv)
-
 make setup
 
-# -----
+# Running application as a service in systemd
 cd /etc/systemd/system
 sudo chown -R ubuntu:ubuntu .
 
