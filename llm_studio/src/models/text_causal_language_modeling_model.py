single sample training -> TODO correct batch paddings, masking, cutting

llm_studio/src/models/text_causal_language_modeling_model.py
@@ -168,22 +168,18 @@ class Model(nn.Module):
 
         self.loss_fn = self.cfg.training.loss_class.get(self.cfg.training.loss_function)
 
-    def generate(self, batch: Dict, cfg: Any):
+    def generate(self, batch: Dict, cfg: Any, remove_prompt=False):
         pad_token_id = (
             self.backbone.config.pad_token_id or self.backbone.config.eos_token_id
         )
 
         if "prompt_attention_mask" in batch:
-            input_ids = batch["prompt_input_ids"]
-            attention_mask = batch["prompt_attention_mask"]
             mask_key = "prompt_attention_mask"
             pad_keys = [
                 "prompt_input_ids",
                 "prompt_attention_mask",
             ]
         else:
-            input_ids = batch["input_ids"]
-            attention_mask = batch["attention_mask"]
             mask_key = "attention_mask"
             pad_keys = [
                 "input_ids",
@@ -197,6 +193,13 @@ class Model(nn.Module):
             mask_key=mask_key,
             pad_keys=pad_keys,
         )
+
+        if "prompt_attention_mask" in batch:
+            input_ids = batch["prompt_input_ids"]
+            attention_mask = batch["prompt_attention_mask"]
+        else:
+            input_ids = batch["input_ids"]
+            attention_mask = batch["attention_mask"]
 
         # Adding GenerationMixin type annotation for faster lookup
         generation_function: GenerationMixin.generate = self.backbone.generate
@@ -231,9 +234,14 @@ class Model(nn.Module):
         )
         transformers_logging.set_verbosity(verbosity)
 
-        # Mask the prompt tokens
-        output[:, : input_ids.shape[1]] = pad_token_id
+        if remove_prompt:
+            # remove the prompt tokens
+            output = output[:, input_ids.shape[1] :]
+        else:
+            # Mask the prompt tokens
+            output[:, : input_ids.shape[1]] = pad_token_id
 
+        logger.info(f"SHAPE: input {input_ids.shape}, output {output.shape}")
         return output
 
     def forward(
@@ -251,16 +259,29 @@ class Model(nn.Module):
 
         # model's forward only works with labels
         if "labels" in batch:
-            batch = batch_padding(
-                self.cfg,
-                batch,
-                self.training,
-                pad_keys=[
+            if "prompt_attention_mask" in batch:
+                mask_key = "prompt_attention_mask"
+                pad_keys = [
+                    "prompt_input_ids",
+                    "prompt_attention_mask",
+                    "special_tokens_mask",
+                    "labels",
+                ]
+            else:
+                mask_key = "attention_mask"
+                pad_keys = [
                     "input_ids",
                     "attention_mask",
                     "special_tokens_mask",
                     "labels",
-                ],
+                ]
+
+            batch = batch_padding(
+                self.cfg,
+                batch,
+                self.training,
+                mask_key=mask_key,
+                pad_keys=pad_keys,
             )
             output = self.backbone(
                 input_ids=batch["input_ids"],
@@ -272,6 +293,29 @@ class Model(nn.Module):
                 assert self.cfg.training.loss_function == "CrossEntropy"
                 outputs["loss"] = output.loss
         else:
+            if "prompt_attention_mask" in batch:
+                mask_key = "prompt_attention_mask"
+                pad_keys = [
+                    "prompt_input_ids",
+                    "prompt_attention_mask",
+                    "special_tokens_mask",
+                ]
+            else:
+                mask_key = "attention_mask"
+                pad_keys = [
+                    "input_ids",
+                    "attention_mask",
+                    "special_tokens_mask",
+                ]
+
+            batch = batch_padding(
+                self.cfg,
+                batch,
+                self.training,
+                mask_key=mask_key,
+                pad_keys=pad_keys,
+            )
+
             output = self.backbone(
                 input_ids=batch["input_ids"],
                 attention_mask=batch["attention_mask"],
@@ -287,7 +331,8 @@ class Model(nn.Module):
 
             outputs["logits"] = output.logits
             outputs["value"] = self.v_head(last_hidden_state).squeeze(-1)
-
+            print("value", outputs["value"].shape)
+            print("logits", outputs["logits"].shape)
         if not self.training or generate:
             outputs["predicted_answer_ids"] = self.generate(batch, self.cfg).detach()
         return outputs

train.py
@@ -279,20 +279,39 @@ def run_train(
                 log_plot(cfg, plot, "train_data")
 
             if cfg.training.use_rlhf:
+                logger.info("Generating response")
+                output_dict = {}
                 with torch.no_grad():
-                    output_dict = model.forward(batch, generate=True)
-
+                    output_dict["predicted_answer_ids"] = model.generate(
+                        batch, model.cfg, remove_prompt=True
+                    ).detach()
                 output_dict = train_dataloader.dataset.postprocess_batch_predictions(
                     cfg=cfg, output=output_dict
                 )
-                # tokenize prompt & output
-                scores = reward_model.get_score(
-                    batch["raw_prompt_text"], output_dict["predicted_text"]
-                )
+
+                logger.info("Reward model")
+                # tokenize prompt & output internally
+                with torch.no_grad():
+                    scores = reward_model.get_score(
+                        batch["raw_prompt_text"], output_dict["predicted_text"]
+                    )
 
                 # score by reward model
                 reward = [torch.tensor(score, dtype=torch.float32) for score in scores]
-                query_tensor = [input_ids for input_ids in batch["input_ids"]]
+
+                # logger.info(
+                #     f"SHAPE: input {batch['input_ids'].shape}, output {output_dict['predicted_answer_ids'].shape}"
+                # )
+
+                for i in range(len(reward)):
+                    print(batch["raw_prompt_text"][i])
+                    print(output_dict["predicted_text"][i])
+                    print("reward", reward[i])
+
+                idx = torch.where(batch["attention_mask"] == 1)[1]
+                query_tensor = [
+                    input_ids[id_:] for input_ids, id_ in zip(batch["input_ids"], idx)
+                ]
                 response_tensor = [
                     predicted_answer_ids
                     for predicted_answer_ids in output_dict["predicted_answer_ids"]