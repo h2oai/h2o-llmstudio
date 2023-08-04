052d7b3188946005ac2e4ce5eeabdbe9ab2b95ec
fatihozturkh2o <fatih.ozturk@h2o.ai>
2023-08-04 13:49:25 +0000

    saving hf yaml separately:X hot fix

--- llm_studio/python_configs/text_causal_language_modeling_config.py
+++ llm_studio/python_configs/text_causal_language_modeling_config.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import os
 from dataclasses import dataclass, field
-from typing import Any, Optional, Tuple
+from typing import Any, Tuple
 
 import torch
 
@@ -461,23 +461,6 @@ class ConfigNLPCausalLMLogging(DefaultConfig):
 
 
 @dataclass
-class ConfigNLPCausalLMHF(DefaultConfig):
-    account_name: str = ""
-    model_name: str = ""
-
-    @property
-    def repo_id(self) -> Optional[str]:
-        if self.account_name != "" and self.model_name != "":
-            return f"{self.account_name}/{self.model_name}"
-        else:
-            return None
-
-    def __post_init__(self):
-        super().__post_init__()
-        self._visibility = {k: -1 for k in self.__dict__}
-
-
-@dataclass
 class ConfigProblemBase(DefaultConfig):
     output_directory: str = f"output/{os.path.basename(__file__).split('.')[0]}"
     experiment_name: str = field(default_factory=generate_experiment_name)
@@ -502,7 +485,6 @@ class ConfigProblemBase(DefaultConfig):
         default_factory=ConfigNLPCausalLMEnvironment
     )
     logging: ConfigNLPCausalLMLogging = field(default_factory=ConfigNLPCausalLMLogging)
-    hf: ConfigNLPCausalLMHF = field(default_factory=ConfigNLPCausalLMHF)
 
     def __post_init__(self):
         super().__post_init__()
@@ -558,5 +540,4 @@ class ConfigProblemBase(DefaultConfig):
                 cfg_dict.get("environment", {})
             ),
             logging=ConfigNLPCausalLMLogging.from_dict(cfg_dict.get("logging", {})),
-            hf=ConfigNLPCausalLMHF.from_dict(cfg_dict.get("hf", {})),
         )
