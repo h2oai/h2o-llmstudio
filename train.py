1368f343b97e6dd789533d08bc5ff87fb80c043e
maxjeblick <maximilianjeblick@gmail.com>
2023-08-07 20:30:35 +0200

    fix autorefactroing

--- train.py
+++ train.py
@@ -522,7 +522,11 @@ def run_train_rlhf(
             with torch.no_grad():
                 logger.debug("Rollout: Generating response from active model")
                 output_dict = {}
-                output_dict["predicted_answer_ids"] = unwrap_model(model).detach()
+                output_dict["predicted_answer_ids"] = (
+                    unwrap_model(model)
+                    .generate(batch, unwrap_model(model).cfg)
+                    .detach()
+                )
                 output_dict = train_dataloader.dataset.postprocess_batch_predictions(
                     cfg=cfg, output=output_dict
                 )
