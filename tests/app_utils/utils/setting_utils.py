a0ecff5113278052ec72d3cb5a352ad49afe5a69
maxjeblick <maximilianjeblick@gmail.com>
2023-08-10 13:15:08 +0200

    add tests for settings

---
+++ tests/app_utils/utils/setting_utils.py
@@ -0,0 +1,63 @@
+from unittest import mock
+
+from app_utils.config import default_cfg
+from app_utils.utils.setting_utils import (
+    NoSaver,
+    KeyRingSaver,
+    EnvFileSaver,
+    Secrets,
+    load_default_user_settings,
+)
+
+
+def test_no_saver():
+    saver = NoSaver("test_user", "/")
+    assert saver.save("name", "password") is None
+    assert saver.load("name") is None
+    assert saver.delete("name") is None
+
+
+def test_keyring_saver(mocker):
+    mocker.patch("keyring.set_password")
+    mocker.patch("keyring.get_password", return_value="password")
+    mocker.patch("keyring.delete_password")
+    saver = KeyRingSaver("test_user", "/")
+    saver.save("name", "password")
+    assert saver.load("name") == "password"
+    saver.delete("name")
+    assert mocker.patch("keyring.delete_password").is_called
+
+
+def test_env_file_saver(tmpdir):
+    saver = EnvFileSaver("test_user", str(tmpdir))
+    saver.save("name", "password")
+    saver.save("name2", "password2")
+    assert saver.load("name") == "password"
+    saver.delete("name")
+    assert saver.load("name") is None
+    assert saver.load("name2") == "password2"
+
+
+def test_secrets_get():
+    assert isinstance(Secrets.get("Do not save credentials permanently"), type)
+    assert isinstance(Secrets.get("Keyring"), type)
+    assert isinstance(Secrets.get(".env File"), type)
+
+
+def test_load_default_user_settings(mocker):
+    q = mock.MagicMock()
+    q.client = dict()
+    mocker.patch("app_utils.utils.setting_utils.clear_secrets", return_value=None)
+    load_default_user_settings(q)
+    assert set(q.client.keys()) == set(default_cfg.user_settings.keys())
+
+
+# You would need to mock the Q class methods to test:
+# save_user_settings
+# load_user_settings
+# load_secrets
+# load_default_user_settings
+# get_secrets_handler
+# clear_secrets
+# maybe_migrate_to_yaml
+# get_usersettings_path
