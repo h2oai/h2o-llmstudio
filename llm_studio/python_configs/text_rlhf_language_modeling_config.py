49a4c0d955af1b7675af5d952600fe64fead0ce4
Pascal Pfeiffer <pascal.pfeiffer@h2o.ai>
2023-08-16 14:45:33 +0200

    fix visibility

--- llm_studio/python_configs/text_rlhf_language_modeling_config.py
+++ llm_studio/python_configs/text_rlhf_language_modeling_config.py
@@ -50,7 +50,7 @@ class ConfigRLHFLMTraining(ConfigNLPCausalLMTraining):
     loss_function: str = "RLHF"
     rollout_steps: int = 64
     adaptive_kl_control: bool = True
-    full_kl_penalty = True
+    full_kl_penalty: bool = True
     initial_kl_coefficient: float = 0.2
     kl_target: float = 6.0
     kl_horizon: int = 10000
