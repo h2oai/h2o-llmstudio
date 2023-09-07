614a0090f76b00162dea0c9c23c9bbcc4af40720
Philipp Singer <killver@gmail.com>
2023-09-07 13:02:28 +0000

    adjustments

--- llm_studio/src/utils/modeling_utils.py
+++ llm_studio/src/utils/modeling_utils.py
@@ -714,11 +714,13 @@ def prepare_lora(cfg, backbone):
         target_modules = []
         for name, module in backbone.named_modules():
             if isinstance(module, (torch.nn.Linear, torch.nn.Conv1d)) and 'head' not in name:
-                name = name.split('.')[-1]
+                # peft checks with .endswith('name') so we need to add the dot
+                name = "." + name.split('.')[-1] if '.' in name else name
                 if name not in target_modules:
                     target_modules.append(name)
-        if cfg.environment._local_rank == 0:
-            logger.info(f"Lora module names: {target_modules}")
+                    
+    if cfg.environment._local_rank == 0:
+        logger.info(f"Lora module names: {target_modules}")
         
     lora_config = LoraConfig(
         r=cfg.training.lora_r,
