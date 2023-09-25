fix quantile computation

llm_studio/app_utils/sections/histogram_card.py
@@ -74,8 +74,10 @@ def compute_quantile_df(x: List[int], a: float, b: float):
         .reset_index()
         .sort_values(by=x_axis_description)[[x_axis_description, "count"]]
     )
-    first_quantile = np.quantile(df_quantile[x_axis_description], a)
-    last_quantile = np.quantile(df_quantile[x_axis_description], b)
+    sorted_data = sorted(x)
+    first_quantile = sorted_data[int(len(sorted_data) * a)]
+    last_quantile = sorted_data[-int(len(sorted_data) * (1 - b))]
+
     df_first = df_quantile.loc[df_quantile[x_axis_description] <= first_quantile].copy()
     df_first["data_type"] = f"first {int(a * 100)}% quantile"
     df_last = df_quantile.loc[df_quantile[x_axis_description] >= last_quantile].copy()

tests/app_utils/sections/histogram_card.py
@@ -1,11 +1,13 @@
 import random
 
+import numpy as np
+
 from llm_studio.app_utils.sections.histogram_card import compute_quantile_df
 
 
 def test_quantiles_are_computed_correctly():
     for _ in range(5):
-        data = random.sample(range(1, 100_000), 10_000)
+        data = np.random.random_integers(0, 1000, 100_000).tolist()
         a = round(random.uniform(0, 1), 2)
         b = round(random.uniform(a, 1), 2)
         a, b = min(a, b), max(a, b)