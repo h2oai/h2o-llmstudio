37d376cbdd6537e5996ba78afbe5ac24b6d4ec74
maxjeblick <maximilianjeblick@gmail.com>
2023-10-17 11:33:59 +0200

    fix datasets that have empty answers

--- llm_studio/src/plots/text_causal_language_modeling_plots.py
+++ llm_studio/src/plots/text_causal_language_modeling_plots.py
@@ -114,7 +114,7 @@ class Plots:
         # Limit to max 15 prompt-conversation-answer rounds
         # This yields to max 5 * sum_{i=1}^{15} i = 600 rows in the DataFrame
         max_conversation_length = min(
-            max([len(conversation["prompts"]) for conversation in conversations] + [1]),
+            max([len(conversation["prompts"]) for conversation in conversations]),
             15,
         )
 
