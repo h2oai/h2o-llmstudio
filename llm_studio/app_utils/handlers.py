788d64cb82dadb710545a515a8aa1a0269b82d9c
maxjeblick <maximilianjeblick@gmail.com>
2023-11-14 23:32:58 +0100

    fix dataset next button not working

--- llm_studio/app_utils/handlers.py
+++ llm_studio/app_utils/handlers.py
@@ -331,8 +331,6 @@ async def handle(q: Q) -> None:
             await dataset_import(q, step=3)
         elif q.args["dataset/import/3/edit"]:
             await dataset_import(q, step=3, edit=True)
-        elif q.args["dataset/import/cfg/train_dataframe"]:
-            await dataset_import(q, step=3)
         elif q.args["dataset/import/4"]:
             await dataset_import(q, step=4)
         elif q.args["dataset/import/4/edit"]:
@@ -347,6 +345,10 @@ async def handle(q: Q) -> None:
             await dataset_merge(q, step=2)
 
         elif q.args["dataset/import/cfg_file"]:
+            await dataset_import(q, step=3)
+
+        # leave at the end
+        elif q.args["dataset/import/cfg/train_dataframe"]:
             await dataset_import(q, step=3)
 
         elif q.args["experiment/start/cfg_file"]:
