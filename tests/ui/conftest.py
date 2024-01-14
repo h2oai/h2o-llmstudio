47e8639f5ede72a54916b7117e9cd65281f19f99
Munish Bhardwaj <munish.bhardwaj@h2o.ai>
2024-01-14 17:33:10 +0530

    Ignore attribute error

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,18 +1,14 @@
-# mypy: begin ignore
 import logging
 from functools import partial, partialmethod
 
 import pytest
 
-logging.TRACE = 5
-logging.addLevelName(logging.TRACE, "TRACE")
-logging.Logger.trace = partialmethod(logging.Logger.log, logging.TRACE)
-logging.trace = partial(logging.log, logging.TRACE)
+logging.TRACE = 5  # type: ignore
+logging.addLevelName(logging.TRACE, "TRACE")  # type: ignore
+logging.Logger.trace = partialmethod(logging.Logger.log, logging.TRACE)  # type: ignore
+logging.trace = partial(logging.log, logging.TRACE)  # type: ignore
 
 
 @pytest.fixture(scope="session")
 def logger() -> logging.Logger:
     return logging.getLogger("ui-tests")
-
-
-# mypy: end ignore
