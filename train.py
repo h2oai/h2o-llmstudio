7c61816ba8d34605d898ee3c9f18e2e7536c52cb
maxjeblick <maximilianjeblick@gmail.com>
2024-01-18 10:06:37 +0100

    fix auto refactroing error

--- train.py
+++ train.py
@@ -559,9 +559,12 @@ def run_train_rlhf(
             with torch.no_grad():
                 logger.debug("Rollout: Generating response from active model")
                 output_dict = {}
-                output_dict["predicted_answer_ids"] = model.generate(
-                    batch, unwrap_model(model).cfg
-                ).detach()
+                output_dict["predicted_answer_ids"] = (
+                    unwrap_model(model)
+                    .generate(batch, unwrap_model(model).cfg)
+                    .detach()
+                )
+
                 output_dict = train_dataloader.dataset.postprocess_batch_predictions(
                     output=output_dict
                 )
