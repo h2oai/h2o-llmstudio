9f137874411ef240a2246b6c8c7853e95c027877
Philipp Singer <killver@gmail.com>
2024-02-08 10:26:24 +0000

    c

--- llm_studio/src/utils/data_utils.py
+++ llm_studio/src/utils/data_utils.py
@@ -673,9 +673,9 @@ def batch_padding(
             ).float()
             quantile = cfg.tokenizer.padding_quantile
         if cfg.environment._distributed:
-            lengths = sync_across_processes(  # noqa: E501
+            lengths = torch.as_tensor(sync_across_processes(
                 lengths, cfg.environment._world_size
-            )
+            ))
         idx = int(torch.floor(torch.quantile(lengths, quantile)))
     else:
         if padding_side == "left":
