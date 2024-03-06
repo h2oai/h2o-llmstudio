cdaf520c6602723d11cd118e662bb3653948c33a
haqishen <haqishen@gmail.com>
2024-03-06 13:52:19 +0900

    make black

--- llm_studio/python_configs/text_causal_language_modeling_config.py
+++ llm_studio/python_configs/text_causal_language_modeling_config.py
@@ -167,12 +167,12 @@ class ConfigNLPCausalLMTraining(DefaultConfig):
         self._possible_values["learning_rate"] = possible_values.Number(
             step=0.000001, min=0.000001
         )
-        self._possible_values[
-            "differential_learning_rate_layers"
-        ] = possible_values.String(
-            values=("backbone", "embed"),
-            allow_custom=False,
-            placeholder="Select optional layers...",
+        self._possible_values["differential_learning_rate_layers"] = (
+            possible_values.String(
+                values=("backbone", "embed"),
+                allow_custom=False,
+                placeholder="Select optional layers...",
+            )
         )
         self._possible_values["differential_learning_rate"] = self._possible_values[
             "learning_rate"
@@ -394,24 +394,24 @@ class ConfigNLPCausalLMEnvironment(DefaultConfig):
         self._possible_values["number_of_workers"] = (1, multiprocessing.cpu_count(), 1)
         self._possible_values["seed"] = possible_values.Number(step=1, min=-1)
         self._possible_values["use_deepspeed"] = ["NA", "ZeRO2", "ZeRO3"]
-        self._possible_values[
-            "deepspeed_allgather_bucket_size"
-        ] = possible_values.Number(step=1, min=1e6)
+        self._possible_values["deepspeed_allgather_bucket_size"] = (
+            possible_values.Number(step=1, min=1e6)
+        )
         self._possible_values["deepspeed_reduce_bucket_size"] = possible_values.Number(
             step=1, min=1e6
         )
-        self._possible_values[
-            "deepspeed_stage3_prefetch_bucket_size"
-        ] = possible_values.Number(step=1, min=1e6)
-        self._possible_values[
-            "deepspeed_stage3_param_persistence_threshold"
-        ] = possible_values.Number(step=1, min=1e6)
-        self._possible_values[
-            "deepspeed_stage3_max_live_parameters"
-        ] = possible_values.Number(step=1, min=1e6)
-        self._possible_values[
-            "deepspeed_stage3_max_reuse_distance"
-        ] = possible_values.Number(step=1, min=1e6)
+        self._possible_values["deepspeed_stage3_prefetch_bucket_size"] = (
+            possible_values.Number(step=1, min=1e6)
+        )
+        self._possible_values["deepspeed_stage3_param_persistence_threshold"] = (
+            possible_values.Number(step=1, min=1e6)
+        )
+        self._possible_values["deepspeed_stage3_max_live_parameters"] = (
+            possible_values.Number(step=1, min=1e6)
+        )
+        self._possible_values["deepspeed_stage3_max_reuse_distance"] = (
+            possible_values.Number(step=1, min=1e6)
+        )
         self._nesting.add(
             [
                 "deepspeed_reduce_bucket_size",
