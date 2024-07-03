9ab5b1e1b38883430ac915e3cf76be46d7d85d32
Philipp Singer <killver@gmail.com>
2024-07-03 13:14:15 +0000

    c

--- llm_studio/python_configs/text_dpo_modeling_config.py
+++ llm_studio/python_configs/text_dpo_modeling_config.py
@@ -76,8 +76,8 @@ class ConfigDPOTraining(ConfigNLPCausalLMTraining):
         self._possible_values["beta"] = possible_values.Number(0.05, 1.0, 0.05)
         self._possible_values["simpo_gamma"] = possible_values.Number(0.05, 2.0, 0.05)
 
-        self._grid_search_values["beta"] = (0.1,0.15,0.2,0.25,0.3)
-        self._grid_search_values["simpo_gamma"] = (0.5, 0.75, 1.0, 1.25, 1.5)
+        self._grid_search_values["beta"] = (0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0	)
+        self._grid_search_values["simpo_gamma"] = (0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0)
 
         self._order.insert("beta", after="learning_rate")
         self._order.insert("simpo_gamma", after="beta")
